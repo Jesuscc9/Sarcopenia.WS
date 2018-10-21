d33f610f720484fcd8c101150a0e4c5338f1ba69
Miguel Sanchez <msanchez@be-it.com.mx>
2018-10-21 17:58:21 -0500

    Agregar modelo Usuario

---
+++ SarcopeniaWS/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SarcopeniaWS.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Emaiil { get; set; }
+        public string Password { get; set; }
+    }
+}
